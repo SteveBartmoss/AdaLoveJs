c90ba547be75e8b36172437ae02d163cb0ab544f
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-02-08 20:13:21 -0600

    Se agrego un nuevo test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { sliceCode } from "./lexer/AdaLove.js"
+import { processCode, sliceCode } from "./lexer/AdaLove.js"
 
 const rawCode = `import React from 'react';\n import './App.css';\n function App() {\n return (\n <div className="App">\n <header className="App-header">\n <h1>¡Hola, React!</h1>\n <p>Bienvenido a tu nueva aplicación creada con Create React App.</p>\n </header>\n </div>\n );\n }\n export default App;\n `
 
+const test = '1+1'
+console.log(processCode(test))
 
-console.log(sliceCode(rawCode))
+//console.log(sliceCode(rawCode))
+
