a3ee7e524907d54c859f5030e5c83735fc2042d3
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-07-12 17:18:45 -0600

    feat: Se agrego un ejemplo con identacion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { processCode, sliceCode } from "./lexer/AdaLove.js"
 
 const rawCode = `import React from 'react';\n import './App.css';\n function App() {\n return (\n <div className="App">\n <header className="App-header">\n <h1>¡Hola, React!</h1>\n <p>Bienvenido a tu nueva aplicación creada con Create React App.</p>\n </header>\n </div>\n );\n }\n export default App;\n `
-const codeTest = 'const persona = { nombre: "Juan" }\n persona.edad = 30; //Añadiendo la propiedad "edad" en tiempo de ejecucion# \n console.log(persona); //nombre: "Juan", edad: 30# \n'
+const codeTest = 'const persona = {\n nombre: "Juan"\n }\n persona.edad = 30; //Añadiendo la propiedad "edad" en tiempo de ejecucion# \n console.log(persona); //nombre: "Juan", edad: 30# \n'
 
 const test = '1+1aaaa/pp/*=-+"hola"'
 
