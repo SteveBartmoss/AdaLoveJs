54e398ad186b3d5298c7134fa375095af10d33d1
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-07-17 21:18:38 -0600

    feat: Se cambio la implementacion para el iterador en el estado 0

--- lexer/lexerStates.js
+++ lexer/lexerStates.js
@@ -7,26 +7,24 @@ export class LexerStates {
         const operators = /([+=\-*/!])/;
         const text = /(['"])/;
         const operatorsAccess = /([:\.])/;
-        const operatorsSet = /([\{\}\[\]\(\)])/
+        const operatorsSet = /([\{\}\[\]\(\)])/;
+
+        iterador++
 
         if (letters.test(char)) {
             swap += char
-            iterador++
             return {    
                 state: 1
             }
         }
         else if (numbers.test(char)) {
             swap += char
-            iterador++
             return {
-                
                 state: 2
             }
         }
         else if (operators.test(char)) {
-            swap += char,
-            iterador++
+            swap += char
 
             if (char === '/') {
                 return {
@@ -40,34 +38,27 @@ export class LexerStates {
         }
         else if (text.test(char)) {
             swap += char
-            iterador++
             return {
                 state: 4
             }
         }
         else if (operatorsAccess.test(char)) {
-            iterador++
             return {
                 token: {typeToken: 'OperatorAcces', character: char,}, 
                 state: 0
             }
         }
         else if (operatorsSet.test(char)) {
-            iterador++
             return {
                 token: {typeToken: 'OperatorSet', character: char,}, 
                 state: 0
             }
         }
         else if (char == ' ') {
-            iterador++
             return {
                 token: {typeToken: 'Espacio', character: char,},
                 state: 0
             }
-        }
-        else {
-            iterador++
         }
 
     }
